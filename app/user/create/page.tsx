7daac438105265383e520602cf028c49d0b61363
levon <Sargsyan088>
2024-11-01 14:05:28 +0400

    initial commit

--- app/user/create/page.tsx
+++ app/user/create/page.tsx
@@ -22,6 +22,7 @@ const CreateUserPage = () => {
         try {
             const response = await axios.post("http://127.0.0.1:8000/api/addnew", userField);
             console.log(response)
+
             window.location.href = '/';
         } catch (err){
             console.log("Something Wrong")
